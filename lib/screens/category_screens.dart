355a1f7997c72ca463f160c867a4bb486e6288c5
TriSetianto123 <156288410+3nol04@users.noreply.github.com>
2025-05-29 23:26:58 +0700

    profile user

---
+++ lib/screens/category_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryScreens extends StatefulWidget {
+  const CategoryScreens({super.key});
+
+  @override
+  State<CategoryScreens> createState() => _CategoryScreensState();
+}
+
+class _CategoryScreensState extends State<CategoryScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
